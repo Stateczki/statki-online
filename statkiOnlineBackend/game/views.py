897960b00408523cf78ce099da8e0e27e2304eae
Hevagog <qkawiak@gmail.com>
2022-11-24 02:01:32 +0100

    Pocopocopoco

--- statkiOnlineBackend/game/views.py
+++ statkiOnlineBackend/game/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+from django.contrib import messages
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 
 
@@ -17,9 +21,24 @@ def profile(request):
     pass
 
 
+def stats(LoginRequiredMixin,):
+    pass
+
+# def register(request):
+#     return render(request,'registrationInterface.tsx')
+
+
 def register(request):
-    return render(request,'registrationInterface.tsx')
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'registrationInterface.tsx', {'form': form})
 
-
-# def login(request):
-#     return render(request,'loggingInterface.tsx')
+def login(request):
+    return render(request,'loggingInterface.tsx')
--- statkiOnlineBackend/statkiOnlineBackend/urls.py
+++ statkiOnlineBackend/statkiOnlineBackend/urls.py
@@ -23,8 +23,13 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', game_views.home, name='statki-home'),
-    path('register/', game_views.register, name='register'),
-    # path('login/', game_views.login, name='login'),
+    path('register/', game_views.register.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='loggingInterface.tsx'), name='login'),
+    path('lobby', game_views.lobby.as_view(), name='lobby'),
+    path('play', game_views.play.as_view(), name='play'),
+    path('game<int:gameID>', game_views.game.as_view(), name='game'),
+    path('<str:username>/homepage', game_views.home.as_view(), name='user-homepage'),
+    path('<str:username>/profile', game_views.home.as_view(), name='user-profile'),
+    path('<str:username>/stats', game_views.stats.as_view(), name='user-stats')
     # path('logout/', auth_views.LogoutView.as_view(template_name='loggingInterface.tsx'), name='logout'),
 ]
