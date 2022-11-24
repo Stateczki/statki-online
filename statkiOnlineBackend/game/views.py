0c9b5a9b22b43233534a0c5301650f6c5a35973d
Hevagog <qkawiak@gmail.com>
2022-11-24 02:06:45 +0100

    Pocopocopoco

--- statkiOnlineBackend/game/views.py
+++ statkiOnlineBackend/game/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+# from .forms import UserRegisterForm
 from django.contrib import messages
 
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -28,17 +28,17 @@ def stats(LoginRequiredMixin,):
 #     return render(request,'registrationInterface.tsx')
 
 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'registrationInterface.tsx', {'form': form})
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserRegisterForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Your account has been created! You are now able to log in')
+#             return redirect('login')
+#     else:
+#         form = UserRegisterForm()
+#     return render(request, 'registrationInterface.tsx', {'form': form})
 
 def login(request):
     return render(request,'loggingInterface.tsx')
