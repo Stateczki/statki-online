e13f7fa93404c40f9a79b7ae7ca4ac468af32822
Hevagog <qkawiak@gmail.com>
2022-11-30 10:55:34 +0100

    Registration_fixed

--- statkiOnlineBackend/users/models.py
+++ statkiOnlineBackend/users/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
--- statkiOnlineBackend/users/views.py
+++ statkiOnlineBackend/users/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 from django.views.decorators.csrf import csrf_exempt
 
