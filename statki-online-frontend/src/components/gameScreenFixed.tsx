8e4c2ca52461f6d4ad3c1b61cf421f7965bba520
Razogarz <patryknapek@gmail.com>
2022-12-26 22:58:17 +0100

    default ustawienie

--- statki-online-frontend/src/components/gameScreenFixed.tsx
+++ statki-online-frontend/src/components/gameScreenFixed.tsx
@@ -1,6 +1,8 @@
+import {useEffect} from 'react'
 export default function gameScreenFixed() {
-    const board = () => {
-        let array = [1,2,3,4,5,6,7,8,9,10]
+    const board = (player:string) => {
+        let array = [1,2,3,4,5,6,7,8,9,10];
+        let defaultShips = [1,2,3,4,21,22,23,25,26,27,41,42,44,45,47,48,61,63,65,67];
         let iterator = 1;
         return <>
             <div className="field-h"></div>
@@ -18,14 +20,17 @@ export default function gameScreenFixed() {
                     return <>
                         <div className="field-h" key={"0_"+colEl}>{colEl}</div>
                         {array.map((rowEl)=>{
-                            return <div className="field" id={""+iterator} key={iterator++}></div>
+                            if(defaultShips.includes(iterator) && player === "player")
+                                return <div className="large ship" key={colEl+"_"+rowEl} id={""+iterator++} onDragStart={drag} draggable="true"></div>
+                            else
+                                return <div className="field" key={colEl+"_"+rowEl} id={""+iterator++}></div>
                         })}
                     </>
                 }
             )}
             </>
     }
-
+    const drag = (e:any) => {console.log(e.target.id)}
     return (
         <>
         <div id="start">
@@ -34,10 +39,10 @@ export default function gameScreenFixed() {
 
         <div id="boards">
             <div id="user-board">
-                {board()}  
+                {board("player")}  
             </div>
             <div id="computer-board">
-                {board()}  
+                {board("enemy")}  
             </div>
         </div>
         
