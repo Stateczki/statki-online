4ee13ff6c7fde4115570f9dd389225c335f10566
Razogarz <patryknapek@gmail.com>
2023-01-04 21:25:47 +0100

    loading player data on profile complete

--- statki-online-frontend/src/components/profilePage.tsx
+++ statki-online-frontend/src/components/profilePage.tsx
@@ -1,15 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function ProfilePage(){
-    
-    let userData = fetch('http://127.0.0.1:8000/userInfo/')
-      .then(response => response.json());
-    
-     //ACCOUNT STATS
-     let username = ""
-     let email = "polak@gmail.va";
-     let playerId = 2137911420;
-     let premium = "yes";
+    //ACCOUNT STATS
+    const [username, setUsername] = useState("default");
+    const [email, setEmail] = useState("default");
+    let playerId = 2137911420;
+    let premium = "yes";
+    let userData : any;
+    fetch('http://127.0.0.1:8000/userInfo/')
+        .then(response => response.json())
+        .then(data => {
+            setUsername(data.username);
+            setEmail(data.email);
+        });
     return(
         <div className="text-xl text-center grid justify-self-center">
             <h1 className="underline-offset-4"><u>Account details:</u></h1>
--- statki-online-frontend/src/components/weatherAndIcon.tsx
+++ statki-online-frontend/src/components/weatherAndIcon.tsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from "react";
 import profilowe from "../profilowe.jpg"
 export default function WeatherAndIcon(){
+    //user parameters
+    let userData : any;
     let temp:number = 22;
     let sky:string = "Sunny";
     let humidity:number = 80;
     let rainfall:string = "None at all";
     let atmosphericPressure = 1019;
+    const [image, setImage] = useState(profilowe);
+
+    //fetch section
+    fetch('http://127.0.0.1:8000/userInfo/')
+        .then(response => response.json())
+        .then(data => {
+            setImage(data.image);
+        });
     return(
         <main className="flex justify-around m-20">
             <div>
-                <img src={profilowe} className="w-56 rounded-full"></img>
+                <img src={image} className="w-56 rounded-full"></img>
             </div>
             <div className="text-xl">
                 <h1 className="underline-offset-4"><u>Today's Weather:</u></h1>
@@ -17,7 +28,7 @@ export default function WeatherAndIcon(){
                     <li>Sky: {sky}</li>
                     <li>Humidity: {humidity}%</li>
                     <li>Rainfall: {rainfall}</li>
-                    <li>Atmospheric pressure: {1019} hPa</li>
+                    <li>Atmospheric pressure: {atmosphericPressure} hPa</li>
                 </ul>
             </div>
         </main>
